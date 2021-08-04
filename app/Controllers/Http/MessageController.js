aefe352fa50e24ea6dba1f4b9c9395406ecffd37
yakrash <kraash@bk.ru>
2021-08-04 20:39:28 +0300

    refactor

--- app/Controllers/Http/MessageController.js
+++ app/Controllers/Http/MessageController.js
@@ -5,77 +5,21 @@
 /** @typedef {import('@adonisjs/framework/src/Response')} Response */
 /** @typedef {import('@adonisjs/framework/src/View')} View */
 
-/**
- * Resourceful controller for interacting with messages
- */
-
 const Message = use('App/Models/Message');
 const TelegrafBot = use('App/service/TelegrafBot');
 
 class MessageController {
-  /**
-   * Show a list of all messages.
-   * GET messages
-   */
-  
-  async index ({ request, response, view }) {
-    return await Message.all();
-  }
 
-  async getById ({ request, response, view }) {
-    console.info(`getById ${request.params.id}`);
-    return await Message.find(request.params.id);
-  }
-
-  /**
-   * Render a form to be used for creating a new message.
-   * GET messages/create
-   */
-  async post ({ request, response, view }) {
+  async post({ request, response, view }) {
     const telegraf = new TelegrafBot();
     const messageNew = new Message();
-    let {text} = request.body;
+    let { text } = request.body;
     messageNew.text = text;
     await messageNew.save();
     console.log(`add messageId ${messageNew.id} with text ${messageNew.text}`);
 
     telegraf.botSend(text);
     return response.json(messageNew);
-  }
-
-  /**
-   * Create/save a new message.
-   * POST messages
-   */
-  async store ({ request, response }) {
-  }
-
-  /**
-   * Display a single message.
-   * GET messages/:id
-   */
-  async show ({ params, request, response, view }) {
-  }
-
-  /**
-   * Render a form to update an existing message.
-   * GET messages/:id/edit
-   */
-  async edit ({ params, request, response, view }) {
-  }
-
-  /**
-   * Update message details.
-   * PUT or PATCH messages/:id
-   */
-  async update ({ params, request, response }) {
-  }
-
-  /**
-   * Delete a message with id.
-   * DELETE messages/:id
-   */
-  async destroy ({ params, request, response }) {
   }
 }
 
--- app/service/telegrafBot.js
+++ app/service/telegrafBot.js
@@ -5,10 +5,8 @@ const bot = new Telegraf('1896607026:AAHwZLY86PvMrZXm9svoHbEpJbq7vo9csFo');
 class TelegrafBot {
     constructor() {
         bot.start(ctx => {
-            ctx.reply("Go Go Go");
-            console.log(ctx.update);
+            ctx.reply("Hello");
         });
-        bot.help(ctx => ctx.reply("Help??"));
         bot.launch()
             .then((res) => {
                 console.log('Bot started');
@@ -17,6 +15,7 @@ class TelegrafBot {
                 console.log(error);
             })
     }
+    
     botSend(msg) {
         bot.telegram.sendMessage(236870885, msg);
     }
