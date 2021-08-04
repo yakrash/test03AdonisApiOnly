ac276643e3f5f8d00ad05484aed16f576428c868
yakrash <kraash@bk.ru>
2021-08-04 15:26:14 +0300

    add controller and routers

---
+++ app/Controllers/Http/MessageController.js
@@ -0,0 +1,76 @@
+'use strict'
+
+/** @typedef {import('@adonisjs/framework/src/Request')} Request */
+/** @typedef {import('@adonisjs/framework/src/Response')} Response */
+/** @typedef {import('@adonisjs/framework/src/View')} View */
+
+/**
+ * Resourceful controller for interacting with messages
+ */
+
+const Message = use('App/Models/Message');
+
+class MessageController {
+  /**
+   * Show a list of all messages.
+   * GET messages
+   */
+  async index ({ request, response, view }) {
+    return await Message.all();
+  }
+
+  async getById ({ request, response, view }) {
+    console.info(`getById ${request.params.id}`);
+    return await Message.find(request.params.id);
+  }
+
+  /**
+   * Render a form to be used for creating a new message.
+   * GET messages/create
+   */
+  async post ({ request, response, view }) {
+    const messageNew = new Message();
+    let {text} = request.body;
+    messageNew.text = text;
+    await messageNew.save();
+    console.log(`add messageId ${messageNew.id} with text ${messageNew.text}`);
+    return response.json(messageNew);
+  }
+
+  /**
+   * Create/save a new message.
+   * POST messages
+   */
+  async store ({ request, response }) {
+  }
+
+  /**
+   * Display a single message.
+   * GET messages/:id
+   */
+  async show ({ params, request, response, view }) {
+  }
+
+  /**
+   * Render a form to update an existing message.
+   * GET messages/:id/edit
+   */
+  async edit ({ params, request, response, view }) {
+  }
+
+  /**
+   * Update message details.
+   * PUT or PATCH messages/:id
+   */
+  async update ({ params, request, response }) {
+  }
+
+  /**
+   * Delete a message with id.
+   * DELETE messages/:id
+   */
+  async destroy ({ params, request, response }) {
+  }
+}
+
+module.exports = MessageController
--- start/routes.js
+++ start/routes.js
@@ -1,6 +1,8 @@
 
 'use strict'
 
+const MessageController = require('../app/Controllers/Http/MessageController').default
+
 /*
 |--------------------------------------------------------------------------
 | Routes
@@ -16,25 +18,34 @@
 
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use('Route')
-const fetch = use('node-fetch')
 
-Route.get('/', async () => { 
-  return await load();
-})
-
-Route.get('/:id', async ({params, response}) => { 
-  let jsons = await load();
-  console.log(params.id);
-  let json = await jsons.filter(user => Number(user.id) === Number(params.id)); 
-  console.log(json);
-  return json;
-})
+Route.group(() => {
+  Route.get('/', 'MessageController.index')
+  Route.get('/:id', 'MessageController.getById')
+  Route.post('/', 'MessageController.post')
+}).prefix('rest')
 
 
-async function load() {
-  const uri = 'https://jsonplaceholder.typicode.com/users';
 
-  let response = await fetch(uri);
-  let json = await response.json();
-  return json; 
-}
+
+
+// Route.get('/', async () => { 
+//   return await load();
+// })
+
+// Route.get('/:id', async ({params, response}) => { 
+//   let jsons = await load();
+//   console.log(params.id);
+//   let json = await jsons.filter(user => Number(user.id) === Number(params.id)); 
+//   console.log(json);
+//   return json;
+// })
+
+
+// async function load() {
+//   const uri = 'https://jsonplaceholder.typicode.com/users';
+
+//   let response = await fetch(uri);
+//   let json = await response.json();
+//   return json; 
+// }
